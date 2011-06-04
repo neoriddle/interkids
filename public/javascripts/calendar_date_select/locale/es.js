d33cad7870223c745ddd9d4a0157c712c8a62938
Israel Buitron <israel.buitron@gmail.com>
2011-06-04 18:14:11 -0500

    Fixes for issue 28 (https://github.com/neoriddle/interkids/issues/28). 'es' locale for calendar_date_select added.
    Signed-off-by: Israel Buitron <israel.buitron@gmail.com>

---
+++ public/javascripts/calendar_date_select/locale/es.js
@@ -0,0 +1,10 @@
+Date.weekdays = $w("Do Lu Ma Mi Ju Vi Sa");
+Date.months = $w("Enero Febrero Marzo Abril Mayo Junio Julio Agosto Septiembre Octubre Noviembre Deciembre" );
+
+Date.first_day_of_week = 1
+
+_translations = {
+  "OK": "Ok",
+  "Now": "Ahora",
+  "Today": "Hoy"
+}
